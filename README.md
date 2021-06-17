693057556c3b7ebfbc10274738e0f6da2dd11458
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-06-17 14:21:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# x-radio-snippets
+---
+
+#### Here are all snippets made by community/rcore dev team!
+
+---
+
+locales
+- czech
+- english
+
+---
+
+Snippets 
+- nothing yet, want to be first? Make a pull request!
+
+---
+
+API
+- nothing yet, want to be first? Make a pull request!
---
+++ locales/english/en.lua
@@ -0,0 +1,50 @@
+Text = {
+    BACK = "Back",
+    HIT = "Hit",
+
+    BALL_IN_HAND = "Ball-in-Hand",
+    BALL_IN_HAND_BACK = "Back",
+
+    AIM_LEFT = "Aim left",
+    AIM_RIGHT = "Aim right",
+    AIM_SLOWER = "Aim slower",
+
+    POOL = 'Pool',
+    POOL_GAME = 'Pool game',
+    POOL_SUBMENU = 'Select ball configuration',
+
+    TYPE_8_BALL = '8-ball',
+    TYPE_STRAIGHT = 'Straight pool',
+
+    HINT_SETUP = 'Set up table',
+    HINT_TAKE_CUE = 'Take pool cue',
+    HINT_RETURN_CUE = 'Return pool cue',
+    HINT_HINT_TAKE_CUE = 'To play pool, take pool cue at the pool cue stand',
+    HINT_PLAY = 'Play',
+
+    BALL_IN_HAND_LEFT = 'Left',
+    BALL_IN_HAND_RIGHT = 'Right',
+    BALL_IN_HAND_UP = 'Up',
+    BALL_IN_HAND_DOWN = 'Down',
+    BALL_POCKETED = '%s ball was pocketed',
+    BALL_IN_HAND_NOTIFY = 'Player has taken cue ball in hand',
+
+    BALL_LABELS = {
+        [-1] = 'Cue',
+        [1] = '~y~Solid 1~s~',
+        [2] = '~b~Solid 2~s~',
+        [3] = '~r~Solid 3~s~',
+        [4] = '~p~Solid 4~s~',
+        [5] = '~o~Solid 5~s~',
+        [6] = '~g~Solid 6~s~',
+        [7] = '~r~Solid 7~s~',
+        [8] = 'Black solid 8',
+        [9] = '~y~Striped 9~s~',
+        [10] = '~b~Striped 10~s~',
+        [11] = '~r~Striped 11~s~',
+        [12] = '~p~Striped 12~s~',
+        [13] = '~o~Striped 13~s~',
+        [14] = '~g~Striped 14~s~',
+        [15] = '~r~Striped 15~s~',
+    }
+},
