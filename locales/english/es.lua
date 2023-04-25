c131981cdab49992f2234a528785cdb3843dd8b5
Gustavo Muzyka <93205175+tavomuzyka@users.noreply.github.com>
2023-04-25 16:59:02 -0300

    Create es.lua
    :D!

---
+++ locales/english/es.lua
@@ -0,0 +1,50 @@
+Text = {
+    BACK = "Volver",
+    HIT = "Golpear",
+
+    BALL_IN_HAND = "Bola en Mano",
+    BALL_IN_HAND_BACK = "Volver",
+
+    AIM_LEFT = "Apunta a la izquierda",
+    AIM_RIGHT = "Apunta a la derecha",
+    AIM_SLOWER = "Apunta más lenta",
+
+    POOL = 'Pool',
+    POOL_GAME = 'Juego de Pool',
+    POOL_SUBMENU = 'Selecciona tipo de Juego',
+
+    TYPE_8_BALL = 'Bola 8',
+    TYPE_STRAIGHT = 'Libre',
+
+    HINT_SETUP = 'Configurar Mesa',
+    HINT_TAKE_CUE = 'Agarrar Taco',
+    HINT_RETURN_CUE = 'Dejar Taco',
+    HINT_HINT_TAKE_CUE = 'Para jugar al Pool, tome el taco de Pool en el puesto de tacos de Pool',
+    HINT_PLAY = 'Jugar',
+
+    BALL_IN_HAND_LEFT = 'Izquierda',
+    BALL_IN_HAND_RIGHT = 'Derecha',
+    BALL_IN_HAND_UP = 'Arriba',
+    BALL_IN_HAND_DOWN = 'Abajo',
+    BALL_POCKETED = '%s Bola embocada',
+    BALL_IN_HAND_NOTIFY = 'El jugador ha tomado la bola blanca en la mano.',
+
+    BALL_LABELS = {
+        [-1] = 'Taco',
+        [1] = '~y~Lisa 1~s~',
+        [2] = '~b~Lisa 2~s~',
+        [3] = '~r~Lisa 3~s~',
+        [4] = '~p~Lisa 4~s~',
+        [5] = '~o~Lisa 5~s~',
+        [6] = '~g~Lisa 6~s~',
+        [7] = '~r~Lisa 7~s~',
+        [8] = 'Lisa Negra 8',
+        [9] = '~y~Rayada 9~s~',
+        [10] = '~b~Rayada 10~s~',
+        [11] = '~r~Rayada 11~s~',
+        [12] = '~p~Rayada 12~s~',
+        [13] = '~o~Rayada 13~s~',
+        [14] = '~g~Rayada 14~s~',
+        [15] = '~r~Rayada 15~s~',
+    }
+},
